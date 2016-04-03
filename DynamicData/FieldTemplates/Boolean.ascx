bf30162e2f72e034f376c9570212f28cb8a43e71
reza bashiri <rzbashiri@gmail.com>
2016-04-03 17:06:43 +0430

    install model and enable dynamicdata

---
+++ DynamicData/FieldTemplates/Boolean.ascx
@@ -0,0 +1,4 @@
+<%@ Control Language="C#" CodeBehind="Boolean.ascx.cs" Inherits="Golestane_Shohada.BooleanField" %>
+
+<asp:CheckBox runat="server" ID="CheckBox1" Enabled="false" />
+
---
+++ Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+ 
+using System.Web.DynamicData;
+using System.Web.Routing;
+namespace Golestane_Shohada
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+       
+            WebUtility.AppStart.EntityModelHelpers.RegisterEntityModel(AppStart.DynamicDataConfig.DefualtModel, new Golestane_Shohada.Model.GolestanShohadaEntities());
+            WebUtility.AppStart.ConfigRoutes.RegisterModelRoutes(RouteTable.Routes, AppStart.DynamicDataConfig.DefualtModel);
+            //WebUtility.AppStart.EntityModelHelpers.RegisterEntityModel(global::AccessManagementService.AppStart.DynamicDataConfig.AccessManagementModel, new global::AccessManagementService.Model.AccessEntities());
+            //WebUtility.AppStart.ConfigRoutes.RegisterModelRoutes(RouteTable.Routes, global::AccessManagementService.AppStart.DynamicDataConfig.AccessManagementModel);
+        
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["CustomerID"] = "";
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
